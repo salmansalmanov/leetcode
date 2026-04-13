627735388897f9f12c0450e45f4ef1b9f6e6ba54
salmansalmanov <salmanov2904@gmail.com>
2026-04-13 16:29:43 +0400

    problem 3483

---
+++ src/easy/Problem3483.java
@@ -0,0 +1,31 @@
+package easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Problem3483 {
+    public static void main(String[] args) {
+        System.out.println(totalNumbers(new int[]{1, 2, 3, 4}));
+    }
+
+    public static int totalNumbers(int[] digits) {
+        Set<Integer> validNumbers = new HashSet<>();
+
+        for (int i = 0; i < digits.length; i++) {
+            for (int j = 0; j < digits.length; j++) {
+                for (int k = 0; k < digits.length; k++) {
+                    if (i != j && j != k && i != k) {
+                        if (digits[i] != 0) {
+                            if (digits[k] % 2 == 0) {
+                                int number = digits[i] * 100 + digits[j] * 10 + digits[k];
+                                validNumbers.add(number);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return validNumbers.size();
+    }
+}
