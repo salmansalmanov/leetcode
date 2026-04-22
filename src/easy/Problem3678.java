64df0bce9afb89c619a06df74a2874b1b15b7863
salmansalmanov <salmanov2904@gmail.com>
2026-04-22 22:44:10 +0400

    problem 3678

---
+++ src/easy/Problem3678.java
@@ -0,0 +1,31 @@
+package easy;
+
+public class Problem3678 {
+    public static void main(String[] args) {
+        System.out.println(smallestAbsent(new int[]{3, 5}));
+    }
+
+    public static int smallestAbsent(int[] nums) {
+        double sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        double avg = sum / nums.length;
+        int count = 1;
+        while (true) {
+            if (count > avg) {
+                boolean found = false;
+                for (int num : nums) {
+                    if (num == count) {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    return count;
+                }
+            }
+            count++;
+        }
+    }
+}
