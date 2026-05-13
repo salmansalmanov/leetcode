00b7a1177e17b11e76be11e42fd8d43027d5b362
salmansalmanov <salmanov2904@gmail.com>
2026-05-13 20:53:12 +0400

    problem 2194

---
+++ src/easy/Problem2194.java
@@ -0,0 +1,24 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem2194 {
+    public static void main(String[] args) {
+        System.out.println(cellsInRange("A1:F1"));
+    }
+
+    public static List<String> cellsInRange(String s) {
+        List<String> result = new ArrayList<>();
+
+        for (char i = s.charAt(0); i <= s.charAt(3); i++) {
+            for (char j = s.charAt(1); j <= s.charAt(4); j++) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(i).append(j);
+                result.add(sb.toString());
+            }
+        }
+
+        return result;
+    }
+}
