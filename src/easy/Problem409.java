9d96adf8fcb65cde435676f20966d86149ed1b9e
salmansalmanov <salmanov2904@gmail.com>
2026-05-14 21:05:25 +0400

    problem 409

---
+++ src/easy/Problem409.java
@@ -0,0 +1,35 @@
+package easy;
+
+public class Problem409 {
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("bb"));
+    }
+
+    public static int longestPalindrome(String s) {
+        char[] arr = s.toCharArray();
+        int[] count = new int[128];
+        int result = 0;
+        boolean hasOdd = false;
+
+        for (char c : arr) {
+            count[c]++;
+        }
+
+        for (int j : count) {
+            if (j == 0) {
+                continue;
+            }
+            if (j % 2 == 0) {
+                result += j;
+            } else {
+                result += j - 1;
+                hasOdd = true;
+            }
+        }
+
+        if (hasOdd) {
+            result++;
+        }
+        return result;
+    }
+}
