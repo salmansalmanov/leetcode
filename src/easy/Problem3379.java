81fede69d26298d050c052242ddf6830156e6e47
salmansalmanov <salmanov2904@gmail.com>
2026-05-15 21:08:31 +0400

    problem 3379

---
+++ src/easy/Problem3379.java
@@ -0,0 +1,27 @@
+package easy;
+
+import java.util.Arrays;
+
+public class Problem3379 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(constructTransformedArray(
+                new int[]{3,-2,1,1}
+        )));
+    }
+
+    public static int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int index = (i + nums[i]) % n;
+
+            if (index < 0) {
+                index += n;
+            }
+
+            result[i] = nums[index];
+        }
+        return result;
+    }
+}
