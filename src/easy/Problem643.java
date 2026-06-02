f2994bf8133befa8b2cbcaef92e5eac4406757c0
salmansalmanov <salmanov2904@gmail.com>
2026-06-02 21:58:30 +0400

    problem 643

---
+++ src/easy/Problem643.java
@@ -0,0 +1,22 @@
+package easy;
+
+public class Problem643 {
+    public static void main(String[] args) {
+        System.out.println(findMaxAverage(new int[]{1, 12, -5, -6, 50, 3}, 4));
+    }
+
+    public static double findMaxAverage(int[] nums, int k) {
+        int n = nums.length;
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        int maxSum = sum;
+
+        for (int i = k; i < n; i++) {
+            sum = sum - nums[i - k] + nums[i];
+            maxSum = Math.max(sum, maxSum);
+        }
+        return maxSum / (double) k;
+    }
+}
