2dff6c6d91cbb43ca8b59cf3d50aa134d535d59c
salmansalmanov <salmanov2904@gmail.com>
2026-08-29 13:00:17 +0400

    problem 3658

---
+++ src/Problem3658.java
@@ -0,0 +1,9 @@
+public class Problem3658 {
+    public static void main(String[] args) {
+        System.out.println(gcdOfOddEvenSums(4));
+    }
+
+    public static int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
