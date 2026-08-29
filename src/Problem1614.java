03ff77adbb8689890992436dc34e9804d6dc10e1
salmansalmanov <salmanov2904@gmail.com>
2026-08-29 12:54:18 +0400

    problem 1614

---
+++ src/Problem1614.java
@@ -0,0 +1,19 @@
+public class Problem1614 {
+    public static void main(String[] args) {
+        System.out.println(maxDepth("()(())((()()))"));
+    }
+
+    public static int maxDepth(String s) {
+        int max = 0;
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                count++;
+                max = Math.max(max, count);
+            } else if (s.charAt(i) == ')') {
+                count--;
+            }
+        }
+        return max;
+    }
+}
